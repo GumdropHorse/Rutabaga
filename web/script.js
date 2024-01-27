e938762efc87250d52a36e983795b6462e4cb750
dtroyermichigan <dtroyermichigan@gmail.com>
2024-01-27 16:35:25 -0500

    Website main page

---
+++ web/script.js
@@ -0,0 +1,7 @@
+ document.getElementById('startButton').addEventListener('click', function() {
+    document.getElementById('titlePage').style.display = 'none';
+    document.getElementById('additionalContent').style.display = 'block';
+
+    // Uncomment the following line to redirect to a different HTML page
+    // window.location.href = 'other-page.html';
+});
