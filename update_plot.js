a38384dea4015ef5be0d1d5a2211505149ecd198
Nathan <LilFuze9@gmail.com>
2024-01-27 20:49:46 -0500

    now in js

--- update_plot.js
+++ update_plot.js
@@ -1,24 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Sample data (replace with your actual data)
-    const plotData = {
-        xData: [1, 2, 3, 4],
-        yData: [1, 1, 0, 0],
-        imageUrls: [
-            'images/rose.png',
-            'images/rose.png',
-            'images/rose.png',
-            'images/rose.png'
-        ]
-    };
+    const selectedPlants = ['sunflower', 'rose', 'rudabaga', 'sunflower', 'rose']
 
     // Display the plot using Plotly
-    displayPlot(plotData);
+    plants = createPlantsDataFrame(selectedPlants);
+    displayPlot(plants);
 });
 
-function displayPlot(plotData) {
+
+function createPlantsDataFrame(selectedPlants) {
+    tallPlants = ['sunflower']
+
+    // Create plants dataFrame
+    const plantNum = selectedPlants.length;
+    const imageList = [];
+    const yCoord = new Array(plantNum).fill(0);
+
+    for (let i = 0; i < plantNum; i++) {
+        const plant = selectedPlants[i];
+        const image = `images/${plant}.png`;
+        imageList.push(image);
+
+        // If the plant is a tall plant, raise it up
+        if (tallPlants.includes(plant)) {
+            yCoord[i] = 1;
+        }
+    }
+
+    const plants = {
+        'plant_name': selectedPlants,
+        'xData': Array.from({ length: plantNum }, (_, i) => i + 1),
+        'yData': yCoord,
+        'imageUrls': imageList,
+        'info': selectedPlants.map(plant => `Info ${plant}`)
+    };
+
+    return plants;
+}
+
+
+function displayPlot(plants) {
     const trace = {
-        x: plotData.xData,
-        y: plotData.yData,
+        x: plants.xData,
+        y: plants.yData,
         mode: 'markers',
         marker: {
             symbol: 'circle',
@@ -26,10 +50,10 @@ function displayPlot(plotData) {
             color: 'red',
             opacity: 0.7,
             // Set the array of image URLs for each data point
-            images: plotData.imageUrls.map(url => ({
+            images: plants.imageUrls.map(url => ({
                 source: url,
-                x: plotData.xData[plotData.imageUrls.indexOf(url)],
-                y: plotData.yData[plotData.imageUrls.indexOf(url)],
+                x: plants.xData[plants.imageUrls.indexOf(url)],
+                y: plants.yData[plants.imageUrls.indexOf(url)],
                 sizex: 20,
                 sizey: 20,
                 xanchor: 'center',
